289af3f135671657154d425e357954720d41190b
Dmytro Gamza <gamzadmytro@gmail.com>
2020-12-08 00:12:38 +0200

    strange

--- task6DataTypes/index.js
+++ task6DataTypes/index.js
@@ -104,3 +104,8 @@ const sumOfAges = actors.reduce((sum, currentValue) => {
 }, 0);
 
 console.log(sumOfAges);
+
+//---
+const every_nth = (arr, nth) => arr.filter((e, i) => i % nth === nth - 1);
+console.log(every_nth([1, 2, 3, 4, 5, 6], 1));
+

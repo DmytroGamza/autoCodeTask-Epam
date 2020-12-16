b2cc31727c045a3b75d91c10d97f7366447e1ff6
Dmytro Gamza <gamzadmytro@gmail.com>
2020-12-16 14:55:26 +0200

    11

--- fromAdd/snipets.js
+++ fromAdd/snipets.js
@@ -10,3 +10,13 @@ console.log(minimum);
 const arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4];
 const min = Math.min(...arr);
 console.log(min);
+
+//Math.min();
+console.log(Math.min(3, 5, 1, 0));
+console.log(Math.min(-4, -8, 2));
+const array1 = [2, 4, 7, -2];
+console.log(Math.min(...array1));
+let x = 3, y = -2;
+let z = Math.min(x, y);
+console.log(z);
+//const x1 = Math.min(f(foo), boundary);
